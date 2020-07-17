98efaf6a1795154156e9a0f93023afde39a5f35a
hzt <ga503306@yahoo.com.tw>
2020-07-17 11:43:59 +0800

    早上改的
    css

---
+++ Admin/Account/Account.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/masterpage/MasterPage.master" AutoEventWireup="true" CodeFile="Account.aspx.cs" Inherits="Admin_Account_Account" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <div class="page-title-box">
+        <h4 id="head_title" class="page-title">變更密碼</h4>
+        <div class="clearfix"></div>
+    </div>
+    <div class="row">
+        <div id="view" class="col-md-12 col-sm-9">
+            <div class="white-box list2">
+                <div class="col-lg-5 m-t-5 p-l-0 p-r-0">
+                    <div class="col-lg-12">
+                        <%--<a class="btn btn-info" href="News.aspx?type=news"><i class="ti-arrow-left"></i>&nbsp;<span>回列表</span>&nbsp;</a>--%>
+                        <a class="btn btn-info" onclick="save()"><i class="ti-save"></i>&nbsp;<span>儲存</span>&nbsp;</a>
+                     <%--   <a id="del_btn" class="btn btn-danger" onclick="data_del('')" runat="server"><i class="fa fa-times"></i>&nbsp;<span>刪除</span>&nbsp;</a>--%>
+                    </div>
+                </div>
+
+                <div class="clear"></div>
+
+                <div class="col-lg-12 m-t-5">
+                    <div>
+                        <asp:Button ID="save" runat="server" Text="Button" OnClick="save_Check" Style="display: none" />
+                      <%--  <asp:Button ID="del" runat="server" Text="刪除" OnClick="del_Click" Style="display: none" />--%>
+                    </div>
+                    <div>
+                        <h2 class="header-title">變更密碼</h2>
+                        <hr />
+                    </div>
+                </div>
+
+                <div class="clear"></div>
+                <asp:HiddenField ID="id" runat="server" />
+                <div class="col-lg-12 m-t-5">
+                    <div class="col-lg-3 form-horizontal">
+                        <div class="form-group">
+                            <label class="col-sm-4 control-label">舊密碼:</label>
+                            <div class="col-sm-8">
+                                <input class="form-control" id="pwd" type="password" runat="server" maxlength="50" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12 m-t-5">
+                    <div class="col-lg-3 form-horizontal">
+                        <div class="form-group">
+                            <label class="col-sm-4 control-label">新密碼:</label>
+                            <div class="col-sm-8">
+                                <input class="form-control" id="npwd" type="password" runat="server" maxlength="100" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 form-horizontal">
+                        <div class="form-group">
+                            <label class="col-sm-4 control-label">確認密碼:</label>
+                            <div class="col-sm-8">
+                                <input class="form-control" id="chknpwd" type="password" runat="server" maxlength="100" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
+
---
+++ Admin/Account/Account.aspx.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class Admin_Account_Account : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            if (Session["Username"] == null)
+            {
+                Response.Redirect("../../login.aspx");
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+    protected void pwd_save_Click()
+    {
+        SqlConnection Conn = new SqlConnection();
+        Conn.ConnectionString = ConfigurationManager.ConnectionStrings["sqlString"].ConnectionString;
+        Conn.Open();
+        SqlTransaction tran = Conn.BeginTransaction();
+
+        DataTable dt = new DataTable();
+        try
+        {
+            string CmdString = @"";
+            CmdString = @"update Employee  set Password=@pwd where Username=@Username ";
+            SqlCommand cmd = new SqlCommand(CmdString, Conn, tran);
+            cmd.Parameters.AddWithValue("Username", Session["Username"].ToString());
+            string des_Password = DB_fountion.EncryptDES(npwd.Value);//加密
+            cmd.Parameters.AddWithValue("pwd", des_Password);
+
+            cmd.ExecuteNonQuery();
+            tran.Commit();
+        }
+        catch (Exception ex)
+        {
+            tran.Rollback();
+            DB_string.log("Account:", ex.ToString());
+        }
+        finally
+        {
+            Conn.Close();
+        }
+    }
+
+    #region Button
+    protected void save_Check(object sender, EventArgs e)
+    {
+        pwd_save_Check();
+    } 
+    #endregion
+
+    #region Other
+    protected void pwd_save_Check()
+    {
+        string result_checkpwd = checkpwd();
+        string result_checknewpwd = checknewpwd();
+        if (result_checkpwd == "OK" && result_checknewpwd == "OK")
+        {
+            pwd_save_Click();
+        }
+        else if (result_checkpwd != "OK")
+        {
+            ScriptManager.RegisterStartupScript(Page, GetType(), "alert", "<script>swal('" + result_checkpwd + "')</script>", false);
+        }
+        else
+        {
+            ScriptManager.RegisterStartupScript(Page, GetType(), "alert", "<script>swal('" + result_checknewpwd + "')</script>", false);
+        }
+    }
+
+    //判斷帳號的舊密碼是否輸入正確
+    protected string checkpwd()
+    {
+        string result;
+        SqlConnection ConnSel = new SqlConnection();
+        ConnSel.ConnectionString = ConfigurationManager.ConnectionStrings["sqlString"].ConnectionString;
+        DataTable dt = new DataTable();
+        string SelCmdString = @"";
+        SelCmdString = @"select 'x' from Employee where Username=@Username and Password=@pwd";
+        SqlCommand Selcmd = new SqlCommand(SelCmdString, ConnSel);
+        string des_Password = DB_fountion.EncryptDES(pwd.Value);//加密
+        Selcmd.Parameters.AddWithValue("Username", Session["Username"].ToString());
+        Selcmd.Parameters.AddWithValue("pwd", des_Password);
+
+        ConnSel.Open();
+        SqlDataReader dr = Selcmd.ExecuteReader(CommandBehavior.CloseConnection);
+        dt.Load(dr);
+        if (dt != null && dt.Rows.Count > 0)
+            result = "OK";
+        else
+            result = "舊密碼錯誤";
+        ConnSel.Close();
+        return result;
+    }
+
+    //判斷新密碼與確認新密碼
+    protected string checknewpwd()
+    {
+        string result = "";
+        if (string.IsNullOrEmpty(npwd.Value))
+            result += "新密碼未填 ";
+        if (string.IsNullOrEmpty(chknpwd.Value))
+            result += "確認新密碼未填 ";
+
+        if (!string.IsNullOrEmpty(chknpwd.Value) && !string.IsNullOrEmpty(chknpwd.Value))
+        {
+            if (npwd.Value == chknpwd.Value)
+                result = "OK";
+            else
+                result = "新密碼與確認新密碼不相同";
+        }
+        return result;
+    }
+
+    #endregion
+}
